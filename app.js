81df07972ba76fa1dca008e281931a6705a2eb2b
Dylan Diegano <74933257+RogueIsRed@users.noreply.github.com>
2022-09-20 00:27:02 +0100

    Final Update
    Made some style changes and added things like score tracker and image selector instead of dull buttons.

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const scoreDisplay = document.getElementById('score')
+
 let userChoice
 let computerChoice
 let result
+let userScore = 0
+let ComputerScore = 0
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     getResult()
+    colorChange()
+    keepingScore()
 }))  
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    const randomNumber = Math.floor(Math.random() * 4)
     
     if (randomNumber === 1) {
         computerChoice = 'rock'
@@ -48,3 +54,24 @@ function getResult() {
     }
     resultDisplay.innerHTML = result
 }
+
+function colorChange() {
+    if (result === 'You Win!!') {
+        document.body.style.backgroundColor = 'green'
+    } else if (result === 'You Lose...') { 
+        document.body.style.backgroundColor = 'red'
+    } else if (result === 'It\'s a draw') {
+        document.body.style.backgroundColor = 'white'
+    }
+}
+
+function keepingScore () {
+    if (result === 'You Win!!') {
+        userScore = userScore + 1; 
+    } else if (result === 'You Lose...') {
+        userScore = userScore - 1;
+    }
+
+    scoreDisplay.innerHTML = userScore
+}
+
