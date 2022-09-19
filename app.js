656eac55eb739d31fbf0184314738f4cf29228d6
Dylan Diegano <74933257+RogueIsRed@users.noreply.github.com>
2022-09-19 02:17:00 +0100

    Project
    Main Base of Project

---
+++ app.js
@@ -0,0 +1,50 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+})) 
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'It\'s a draw' 
+    }
+
+    if (computerChoice === 'rock' && userChoice === 'paper' 
+     || computerChoice === 'scissors' && userChoice === 'rock' 
+     || computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'You Win!!' 
+    }
+
+    if (computerChoice === 'scissors' && userChoice === 'paper' 
+     || computerChoice === 'rock' && userChoice === 'scissors' 
+     || computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'You Lose...' 
+    }
+    resultDisplay.innerHTML = result
+}
